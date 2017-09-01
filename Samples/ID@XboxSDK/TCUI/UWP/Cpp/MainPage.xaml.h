761bdcfaa985291bd366f648d0983ceb58d761d7
Ramsey <31288345+rakhad@users.noreply.github.com>
2017-09-01 16:48:47 -0700

    Fixed TCUI sample so it passes WACK (#54)

--- Samples/ID@XboxSDK/TCUI/UWP/Cpp/MainPage.xaml.h
+++ Samples/ID@XboxSDK/TCUI/UWP/Cpp/MainPage.xaml.h
@@ -8,7 +8,7 @@
 
 Platform::String^ StringFormat(LPCWSTR strMsg, ...);
 
-namespace Sample
+namespace TCUI
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
